7198fd2db1d78eda164d66be1291987a9346dcba
sudeephealthplix <106803245+sudeephealthplix@users.noreply.github.com>
2022-06-24 12:24:40 +0530

    come functions more added

--- HealthplixAssignmentSudeepSharma/Server/main.go
+++ HealthplixAssignmentSudeepSharma/Server/main.go
@@ -8,7 +8,9 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"database/sql"
-    _ "github.com/go-sql-driver/mysql"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type album struct {
@@ -27,68 +29,68 @@ var albums = []album{
 func main() {
 	db, err := sql.Open("mysql", "root:SoumiliDas1#@tcp(127.0.0.1:3306)/healthplixAssignmentDB")
 	if err != nil {
-        panic(err.Error())
-    }
-    defer db.Close()
-
+		panic(err.Error())
+	}
+	defer db.Close()
 
 	router := gin.Default()
 	router.GET("/medicine", getMedicines)
 	router.POST("/medicine", postMedicine)
-	router.GET("/medicine/:name", getMedicineByName)
-	router.DELETE("/medicine/:name",deleteMedicineByName)
-	router.PATCH("/medicine/:",updateMedicineByName)
+	//router.GET("/medicine/:name", getMedicineByName)
+	//router.DELETE("/medicine/:name",deleteMedicineByName)
+	//router.PATCH("/medicine/:",updateMedicineByName)
 	router.Run("localhost:8080")
 }
+
 type Timestamp struct {
 	time.Time
 }
 type medicine struct {
-    created_on Timestamp `json:"created_on"`
-	updated_on Timestamp `json:"updated_on"`
-	updated_by string `json:"updated_by"`
-	created_by string `json:"created_by"`
-    medicine_name string `json:"medicine_name"`
-	manufacturer string `json:"manufacturer"`
-	medicine_price int `json:"medicine_price"`
-	medicine_id int` json:"medicine_id"`
+	created_on     string `json:"created_on"`
+	updated_on     string `json:"updated_on"`
+	updated_by     string `json:"updated_by"`
+	created_by     string `json:"created_by"`
+	medicine_name  string `json:"medicine_name"`
+	manufacturer   string `json:"manufacturer"`
+	medicine_price int    `json:"medicine_price"`
+	medicine_id    int    ` json:"medicine_id"`
 }
 type user struct {
-    created_on Timestamp `json:"created_on"`
-	updated_on Timestamp `json:"updated_on"`
-	updated_by string `json:"updated_by"`
-	created_by string `json:"created_by"`
-    user_name string `json:"user_name"`
-	access_level int `json:"access_level"`
-	user_email_id string `json:"user_email_id"`
-	user_password string ` json:"user_password"`
-	user_id int ` json:"user_id"`
+	created_on    Timestamp `json:"created_on"`
+	updated_on    Timestamp `json:"updated_on"`
+	updated_by    string    `json:"updated_by"`
+	created_by    string    `json:"created_by"`
+	user_name     string    `json:"user_name"`
+	access_level  int       `json:"access_level"`
+	user_email_id string    `json:"user_email_id"`
+	user_password string    ` json:"user_password"`
+	user_id       int       ` json:"user_id"`
 }
 
 func getMedicines(c *gin.Context) {
 	//c.IndentedJSON(http.StatusOK, albums)
 	db, err := sql.Open("mysql", "root:SoumiliDas1#@tcp(127.0.0.1:3306)/healthplixAssignmentDB")
 	if err != nil {
-        panic(err.Error())
-    }
-    defer db.Close()
+		panic(err.Error())
+	}
+	defer db.Close()
 	results, err := db.Query("SELECT * FROM medicines")
-    if err != nil {
-        panic(err.Error()) // proper error handling instead of panic in your app
-    }
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
 	medicines_to_return := []medicine{}
 	for results.Next() {
 
-        var medicine_dash medicine
-        // for each row, scan the result into our tag composite object
-        err = results.Scan(&medicine_dash.created_on,&medicine_dash.updated_on,&medicine_dash.updated_by,&medicine_dash.created_by,&medicine_dash.medicine_name,&medicine_dash.manufacturer,&medicine_dash.medicine_price,&medicine_dash.medicine_id)
-        if err != nil {
-            panic(err.Error()) // proper error handling instead of panic in your app
-        }
-                // and then print out the tag's Name attribute
+		var medicine_dash medicine
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&medicine_dash.created_on, &medicine_dash.updated_on, &medicine_dash.updated_by, &medicine_dash.created_by, &medicine_dash.medicine_name, &medicine_dash.manufacturer, &medicine_dash.medicine_price, &medicine_dash.medicine_id)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// and then print out the tag's Name attribute
 		medicines_to_return = append(medicines_to_return, medicine_dash)
-        c.IndentedJSON(http.StatusOK, medicines_to_return)
-    }
+		c.IndentedJSON(http.StatusOK, medicines_to_return)
+	}
 
 }
 
@@ -105,16 +107,24 @@ func getAlbumByID(c *gin.Context) {
 	}
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
-func postAlbums(c *gin.Context) {
-	var newAlbum album
-
-	// Call BindJSON to bind the received JSON to
-	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
+func postMedicine(c *gin.Context) {
+	var newMedicine medicine
+	println("ye body hai "c.Request.Body+" yahi hai bhai bas")
+	if err := c.BindJSON(&newMedicine); err != nil {
 		return
 	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	db, err := sql.Open("mysql", "root:SoumiliDas1#@tcp(127.0.0.1:3306)/healthplixAssignmentDB")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	var query = (`INSERT INTO medicines
+	VALUES (` + newMedicine.created_on + "," + newMedicine.updated_on + "," + newMedicine.updated_by + "," + newMedicine.created_by + "," + newMedicine.medicine_name + "," + newMedicine.manufacturer + "," + strconv.Itoa(newMedicine.medicine_price) + "," + strconv.Itoa((newMedicine.medicine_id)) + ")")
+	println(newMedicine.created_on)
+	results, err := db.Query(query)
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	println(results)
+	c.IndentedJSON(http.StatusCreated, newMedicine)
 }
